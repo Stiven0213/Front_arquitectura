1b64853f4d1dbedbcfa4b274f6a5aec2f96db87e
Stiven Agudelo <stiven.021322@gmail.com>
2024-03-24 12:55:42 -0500

    Se crea la estructura base del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { LandingPageComponent } from './shared/components/landing-page/landing-page.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LandingPageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
